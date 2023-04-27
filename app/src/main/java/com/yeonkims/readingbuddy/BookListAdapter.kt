3f32288f7e63056d4ed3bda60c8c9c6b57b34aab
Jiyeon Kim <jiyeon7269@naver.com>
2023-04-27 18:26:56 +0900

    refactor: findViewById() 대신DataBinding 사용

--- app/src/main/java/com/yeonkims/readingbuddy/BookListAdapter.kt
+++ app/src/main/java/com/yeonkims/readingbuddy/BookListAdapter.kt
@@ -1,34 +1,33 @@
 package com.yeonkims.readingbuddy
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.yeonkims.readingbuddy.databinding.ItemBookBinding
 import com.yeonkims.readingbuddy.model.Book
 
 class BookListAdapter(val items: List<Book>) : RecyclerView.Adapter<BookListAdapter.ViewHolder>() {
 
+    private lateinit var binding: ItemBookBinding
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
-        return ViewHolder(view)
+        binding = ItemBookBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        holder.title.text = item.title
-        holder.author.text = item.author
-        holder.image.setImageResource(item.image)
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title: TextView = itemView.findViewById(R.id.title_text)
-        val author: TextView = itemView.findViewById(R.id.author_text)
-        val image: ImageView = itemView.findViewById(R.id.book_image)
+    class ViewHolder(val binding: ItemBookBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Book) {
+            binding.book = item
+        }
     }
 }
